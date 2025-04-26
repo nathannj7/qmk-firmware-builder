32e546851ff8bf4bc948d501b28b3b25bf9eccbf
Dave Smith <davepwsmith@users.noreply.github.com>
2025-04-26 17:53:54 +0100

    Add custom-vial firmware for tidbit

---
+++ keyboards/nullbitsco/tidbit/keymaps/custom-vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF6, 0x6D, 0xB4, 0x30, 0x09, 0x62, 0x60, 0x54}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS  { 3, 1 }
+
+/* Add timeout for RGB lights */
+#define RGBLIGHT_SLEEP 
+#define RGBLIGHT_TIMEOUT 60000
